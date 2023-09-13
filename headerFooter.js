4890c7aa4edc8d3c1df58bdc979b29ffd894a7b1
HinYong1031 <hinyong16@gmail.com>
2023-09-13 16:19:02 +0800

    master page implement and fixed footer bug

---
+++ headerFooter.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Load and insert the header and footer into the master.html
+    fetch('master.html')
+        .then(response => response.text())
+        .then(html => {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(html, 'text/html');
+            
+            const header = doc.querySelector('header');
+            const footer = doc.querySelector('footer');
+
+            document.querySelector('header').innerHTML = header.innerHTML;
+            document.querySelector('footer').innerHTML = footer.innerHTML;
+        });
+});
