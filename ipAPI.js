5a8b2583fd0b1cc2c77187d6a4f645a05761576b
Yongyue0826 <109788606+Yongyue0826@users.noreply.github.com>
2023-09-15 18:52:52 +0800

    change country & city order

--- ipAPI.js
+++ ipAPI.js
@@ -2,6 +2,6 @@
 $.getJSON('https://ipinfo.io/json?token=7ef23bc2fbe51a', function(data) {
 $("#ip").text(data.ip); // Display the IPv4 address here
 $('#isp').text(data.org);
-$('#country').text(data.country);
 $('#city').text(data.region);
+$('#country').text(data.country);
 })
